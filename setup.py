aef944d6b420dd735fd6dce687e99d9972b3507d
Adam Tyson <adam.tyson@ucl.ac.uk>
2019-10-14 17:03:00 +0100

    Add setup file

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_namespace_packages
+
+requirements = [
+    "numpy",
+    "pandas",
+    "vtkplotter",
+    "vtk",
+    "jupyter",
+    "allensdk",
+    "k3d",
+    "tqdm",
+]
+
+setup(
+    name="BrainRender",
+    version="0.1.0",
+    description="Python scripts to use Allen Brain Map data for analysis "
+                "and rendering",
+    install_requires=requirements,
+    python_requires=">=3.6",
+    packages=find_namespace_packages(exclude=(
+        "Installation", "Meshes", "Metadata", "Screenshots")),
+    include_package_data=True,
+    url="https://github.com/BrancoLab/BrainRender",
+    author="Federico Claudi",
+    zip_safe=False,
+)
