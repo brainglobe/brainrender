3635eb55a252b84c87a0165943a0104f34a5c458
FedeClaudi <federicoclaudi@protonmail.com>
2020-02-27 12:04:39 +0000

    bump

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ requirements = [
 
 setup(
     name="brainrender",
-    version="0.3.3.6b",
+    version="0.3.3.8",
     description="Python scripts to use Allen Brain Map data for analysis "
                 "and rendering",
     install_requires=requirements,
