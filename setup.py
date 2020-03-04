f1ab02c857ce24d7fa2717599b801f51a4be6d33
FedeClaudi <federicoclaudi@protonmail.com>
2020-03-04 20:36:28 +0000

    updated vtkplotter requirement

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 requirements = [
     "numpy",
     "pandas",
-    "vtkplotter>=2020.0.1",
+    "vtkplotter>=2020.2.1",
     "vtk",
     "allensdk",
     "tqdm",
