cb20a7e1502a3d0745fc6d7a98604b92bfe4b251
FedeClaudi <federicoclaudi@protonmail.com>
2020-04-07 10:08:47 +0100

    video creation works on max

--- Examples/basic_video_maker.py
+++ Examples/basic_video_maker.py
@@ -7,11 +7,11 @@ from brainrender.scene import Scene
 from brainrender.animation.video import BasicVideoMaker 
 
 
-scene = Scene(display_inset=False)
+scene = Scene(display_inset=False, camera="sagittal")
 
 scene.add_brain_regions(['MOs'])
-scene.add_neurons("Examples\example_files\one_neuron.json")
+scene.add_neurons("Examples/example_files/one_neuron.json", soma_color="black")
 
 vmkr = BasicVideoMaker(scene)
 
-vmkr.make_video(azimuth=3.6, niters=10, save_name="test")
+vmkr.make_video(azimuth=1, niters=360, duration=30, save_name="test")
