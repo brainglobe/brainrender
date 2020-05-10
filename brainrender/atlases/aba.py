551f93862f10262eeba160c6c4013f121d4ee2d1
FedeClaudi <federicoclaudi@protonmail.com>
2020-05-10 17:21:42 +0100

    finished add neurons for aba

--- brainrender/atlases/aba.py
+++ brainrender/atlases/aba.py
@@ -16,6 +16,7 @@ from brainrender import STREAMLINES_RESOLUTION, INJECTION_VOLUME_SIZE
 from brainrender.Utils.webqueries import request
 from brainrender import * 
 from brainrender.Utils import actors_funcs
+from brainrender.colors import _mapscales_cmaps, makePalette, get_random_colors, getColor, colors
 
 
 from allensdk.core.mouse_connectivity_cache import MouseConnectivityCache
@@ -231,11 +232,9 @@ class ABA(Atlas):
         else:
             return right
 
+
     @staticmethod # static method because this should inherit from scene
-    def add_neurons(self, neurons, display_soma_region=False, soma_regions_kwargs=None,
-                    display_axon_regions=False,
-                    display_dendrites_regions=False, 
-                    **kwargs):
+    def add_neurons(self, neurons, color=None, display_axon=True, display_dendrites=True):
         """
         Adds rendered morphological data of neurons reconstructions downloaded from the
         Mouse Light project at Janelia (or other sources). 
@@ -246,12 +245,109 @@ class ABA(Atlas):
 
         :param self: instance of brainrender Scene to use to render neurons
         :param neurons: str, list, dict. File(s) with neurons data or list of rendered neurons.
-        :param **kwargs:
+        :param display_axon, display_dendrites: if set to False the corresponding neurite is not rendered
+        :param color: default None. Can be:
+                - None: each neuron is given a random color
+                - color: rbg, hex etc. If a single color is passed all neurons will have that color
+                - cmap: str with name of a colormap: neurons are colored based on their sequential order and cmap
+                - dict: a dictionary specifying a color for soma, dendrites and axon actors, will be the same for all neurons
+                - list: a list of length = number of neurons with either a single color for each neuron
+                        or a dictionary of colors for each neuron
         """
 
         if not isinstance(neurons, (list, tuple)):
             neurons = [neurons]
 
+        # ------------------------------ Prepare colors ------------------------------ #
+        N = len(neurons)
+        colors = dict(
+            soma = None,
+            axon = None,
+            dendrites = None,
+        )
+
+        # If no color is passed, get random colors
+        if color is None:
+            cols = get_random_colors(N)
+            colors = dict(
+                soma = cols.copy(),
+                axon = cols.copy(),
+                dendrites = cols.copy(),)
+        else:
+            if isinstance(color, str):
+                # Deal with a a cmap being passed
+                if color in _mapscales_cmaps:
+                    cols = [colorMap(n, name=color, vmin=-2, vmax=N+2) for n in np.arange(N)]
+                    colors = dict(
+                        soma = cols.copy(),
+                        axon = cols.copy(),
+                        dendrites = cols.copy(),)
+
+                else:
+                    # Deal with a single color being passed
+                    cols = [getColor(color) for n in np.arange(N)]
+                    colors = dict(
+                        soma = cols.copy(),
+                        axon = cols.copy(),
+                        dendrites = cols.copy(),)
+            elif isinstance(color, dict):
+                # Deal with a dictionary with color for each component
+                if not 'soma' in color.keys():
+                    raise ValueError(f"When passing a dictionary as color argument, \
+                                                soma should be one fo the keys: {color}")
+                dendrites_color = color.pop('dendrites', color['soma'])
+                axon_color = color.pop('axon', color['soma'])
+
+                colors = dict(
+                        soma = [color['soma'] for n in np.arange(N)],
+                        axon = [axon_color for n in np.arange(N)],
+                        dendrites = [dendrites_color for n in np.arange(N)],)
+                        
+            elif isinstance(color, (list, tuple)):
+                # Check that the list content makes sense
+                if len(color) != N:
+                    raise ValueError(f"When passing a list of color arguments, the list length"+
+                                f" ({len(color)}) should match the number of neurons ({N}).")
+                if len(set([type(c) for c in color])) > 1:
+                    raise ValueError(f"When passing a list of color arguments, all list elements"+
+                                " should have the same type (e.g. str or dict)")
+
+                if isinstance(color[0], dict):
+                    # Deal with a list of dictionaries
+                    soma_colors, dendrites_colors, axon_colors = [], [], []
+
+                    for col in colors:
+                        if not 'soma' in col.keys():
+                            raise ValueError(f"When passing a dictionary as col argument, \
+                                                        soma should be one fo the keys: {col}")
+                        dendrites_colors.append(col.pop('dendrites', col['soma']))
+                        axon_colors.append(col.pop('axon', col['soma']))
+                        soma_colors.append(col['soma'])
+
+                    colors = dict(
+                        soma = soma_colors,
+                        axon = axon_colors,
+                        dendrites = dendrites_colors,)
+
+                else:
+                    # Deal with a list of colors
+                    colors = dict(
+                        soma = color.copy(),
+                        axon = color.copy(),
+                        dendrites = color.copy(),)
+            else:
+                raise ValueError(f"Color argument passed is not valid. Should be a \
+                                        str, dict, list or None, not {type(color)}:{color}")
+
+        # Check colors, if everything went well we should have N colors per entry
+        for k,v in colors.items():
+            if len(v) != N:
+                raise ValueError(f"Something went wrong while preparing colors. Not all \
+                                entries have right length. We got: {colors}")
+
+
+
+        # ---------------------------------- Render ---------------------------------- #
         _neurons_actors = []
         for neuron in neurons:
             neuron_actors = {'soma':None, 'dendrites':None, 'axon': None}
@@ -290,9 +386,7 @@ class ABA(Atlas):
             
             # Deal with neuron as instance of Neuron from morphapi
             elif isinstance(neuron, Neuron):
-                neuron_actors, _ = get_neuron_actors_with_morphapi(neuron=neuron)
-                a = 2
-                
+                neuron_actors, _ = get_neuron_actors_with_morphapi(neuron=neuron)                
             # Deal with other inputs
             else:
                 raise ValueError(f"Passed neuron {neuron} is not a valid input")
@@ -303,7 +397,20 @@ class ABA(Atlas):
                     if not isinstance(act, Actor):
                         raise ValueError(f"Neuron actor {key} is {act.__type__} but should be a vtkplotter Mesh. Not: {act}")
 
+            if not display_axon:
+                neuron_actors['axon'] = None
+            if not display_dendrites:
+                neuron_actors['dendrites'] = None
             _neurons_actors.append(neuron_actors)
+
+        # Color actors
+        for n, neuron in enumerate(_neurons_actors):
+            if neuron['axon'] is not None:
+                neuron['axon'].c(colors['axon'][n])
+            neuron['soma'].c(colors['soma'][n])
+            if neuron['dendrites'] is not None:
+                neuron['dendrites'].c(colors['dendrites'][n])
+
 
         # Add to actors storage
         self.actors["neurons"].extend(_neurons_actors)
