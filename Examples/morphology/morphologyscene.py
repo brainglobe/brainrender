3f05243cac7e5debd5029cae64c18e20c74b1704
FedeClaudi <federicoclaudi@protonmail.com>
2020-05-15 09:48:04 +0100

    example

--- Examples/morphology/morphologyscene.py
+++ Examples/morphology/morphologyscene.py
@@ -2,20 +2,16 @@
     This tutorial shows how to render a neuron from a .swc file
     with the MorphologyScene class
 """
+from vtkplotter import Text
+
 import brainrender
 brainrender.SHADER_STYLE = 'cartoon'
 
-from brainrender.morphology.visualise import MorphologyScene
+from brainrender.scene import Scene
+
+scene = Scene()
+
+mos = scene.add_brain_regions('MOs')
 
 
-scene = MorphologyScene(title='A neuron')
-
-neuron = scene.add_neurons('Examples/example_files/neuron4.swc',
-                color = dict(
-                    soma = 'red',
-                    dendrites= 'orangered',
-                    axon = [.4, .4, .4],
-                ))
-
-scene.render()
-
+a = 1
