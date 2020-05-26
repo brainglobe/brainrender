09553593fe837c480828920b321f2287667c4484
FedeClaudi <federicoclaudi@protonmail.com>
2020-05-26 14:43:03 +0100

    fix

--- tests/tests/test_integration/test_main.py
+++ tests/tests/test_integration/test_main.py
@@ -10,7 +10,6 @@ from brainrender.scene import Scene, MultiScene
 from brainrender import *
 from brainrender.colors import get_n_shades_of
 from brainrender.atlases.aba import ABA
-from brainrender.Utils.parsers.streamlines import *
 from brainrender.Utils.data_io import listdir
 
 from morphapi.morphology.morphology import Neuron
