367e75010ca82c91d8759aa7f32bb34718e36710
FedeClaudi <federicoclaudi@protonmail.com>
2020-05-31 11:33:32 +0100

    humanatlas

---
+++ brainrender/atlases/human.py
@@ -0,0 +1,13 @@
+from brainatlas_api.bg_atlas import AllenHumanBrain500Um
+
+from brainrender.atlases.brainglobe import BrainGlobeAtlas
+
+
+class BGAllenHumanBrain500Um(BrainGlobeAtlas, AllenHumanBrain500Um):
+    atlas_name = "allen_human_500um_v0.1"
+
+    def __init__(self, base_dir=None, **kwargs):
+        BrainGlobeAtlas.__init__(self, base_dir=base_dir, **kwargs)
+        AllenHumanBrain500Um.__init__(self)
+
+        self.meshes_folder = self.root_dir / "meshes"
