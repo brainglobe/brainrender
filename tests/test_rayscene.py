2f9c0c69253219dba7a070443347aab0f5a19a11
FedeClaudi <federicoclaudi@protonmail.com>
2020-06-30 22:51:23 +0100

    cleanup

--- tests/test_rayscene.py
+++
@@ -1,7 +0,0 @@
-# from brainrender.raytracing_scene import RayScene
-
-
-# def test_rayscene():
-#     scene = RayScene(add_root=True)
-#     scene.add_brain_regions(["MOs", "TH"], alpha=1)
-#     scene.render(show=False)
