4ba30fcc0bf411c72f14198ff36a463c3a9c6a80
alessandrofelder <alessandrofelder@users.noreply.github.com>
2023-11-15 14:22:26 +0000

    modularise export test

--- tests/test_export_html.py
+++ tests/test_export_html.py
@@ -5,15 +5,19 @@ import pytest
 from brainrender import Scene
 
 
-@pytest.mark.local
-def test_export_for_web():
+@pytest.fixture
+def scene():
+    """Provide a scene with a brain region"""
     s = Scene(title="BR")
-
     th = s.add_brain_region("TH")
-
     s.add_label(th, "TH")
+    return s
 
-    path = s.export("test.html")
+
+@pytest.mark.local
+def test_export_for_web(scene):
+    """Check that exporting to html creates the expected file"""
+    path = scene.export("test.html")
     assert path == "test.html"
 
     path = Path(path)
@@ -21,5 +25,9 @@ def test_export_for_web():
 
     path.unlink()
 
+
+@pytest.mark.local
+def test_export_for_web_raises(scene):
+    """Check that exporting with invalid file extention raises ValueError"""
     with pytest.raises(ValueError):
-        path = s.export("test.py")
+        scene.export("test.py")
