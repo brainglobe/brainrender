52b1ec041fa24e20a5609a1238be245638f50b3e
Jingjie Li <jingjie.li@nyu.edu>
2024-01-11 16:59:52 +0000

    example to show probe tracks (#313)
    * example to show probe tracks
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Standardise script
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Adam Tyson <code@adamltyson.com>

---
+++ examples/probe_tracks.py
@@ -0,0 +1,40 @@
+"""
+This example visualizes `.npy` files exported from brainglobe-segmentation
+"""
+
+from pathlib import Path
+import numpy as np
+
+from brainrender import Scene
+from brainrender.actors import Points
+
+data_path = Path(__file__).parent.parent / "brainrender" / "resources"
+
+scene = Scene(title="Silicon Probe Visualization")
+
+# Visualise the probe target regions
+cp = scene.add_brain_region("CP", alpha=0.15)
+rsp = scene.add_brain_region("RSP", alpha=0.15)
+
+# Add probes to the scene.
+# Each .npy file should contain a numpy array with the coordinates of each
+# part of the probe.
+scene.add(
+    Points(
+        np.load(data_path / "probe_1_striatum.npy"),
+        name="probe_1",
+        colors="darkred",
+        radius=50,
+    )
+)
+scene.add(
+    Points(
+        np.load(data_path / "probe_2_RSP.npy"),
+        name="probe_2",
+        colors="darkred",
+        radius=50,
+    )
+)
+
+# render
+scene.render()
