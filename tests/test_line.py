8e4ce50fedfb5dd1f6999cb4372b7b1a7062bcb1
Paul Brodersen <paulbrodersen@gmail.com>
2024-04-17 12:50:32 +0100

    Add Line actor test

---
+++ tests/test_line.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+from brainrender import Scene
+from brainrender.actor import Actor
+from brainrender.actors import Line
+
+def test_line():
+    s = Scene()
+
+    line = Line(
+        np.array([
+            [0, 0, 0],
+            [1, 1, 1],
+            [2, 2, 2],
+        ])
+    )
+
+    s.add(line)
+    assert isinstance(line, Actor)
+
+    del s
