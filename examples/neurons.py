6e694e5189d1767bd7fd43d9d1a4a2783b73dfac
Igor Tatarnikov <igor.tatarnikov@gmail.com>
2024-06-06 10:22:27 +0100

    Added try-except in examples/neurons.py to prevent test failure when neuromorpho is down

--- examples/neurons.py
+++ examples/neurons.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from importlib.resources import files
 
+import requests.exceptions
 from morphapi.api.mouselight import MouseLightAPI
 from myterial import orange
 from rich import print
@@ -19,14 +20,17 @@ scene = Scene(title="neurons")
 scene.add(Neuron(neuron_file))
 
 # Download neurons data with morphapi
-mlapi = MouseLightAPI()
-neurons_metadata = mlapi.fetch_neurons_metadata(
-    filterby="soma", filter_regions=["MOs"]
-)
+try:
+    mlapi = MouseLightAPI()
+    neurons_metadata = mlapi.fetch_neurons_metadata(
+        filterby="soma", filter_regions=["MOs"]
+    )
 
-to_add = [neurons_metadata[47], neurons_metadata[51]]
-neurons = mlapi.download_neurons(to_add)
-neurons = scene.add(*make_neurons(*neurons, neurite_radius=12))
+    to_add = [neurons_metadata[47], neurons_metadata[51]]
+    neurons = mlapi.download_neurons(to_add)
+    neurons = scene.add(*make_neurons(*neurons, neurite_radius=12))
+except ConnectionError as e:
+    print("Failed to download neurons data from neuromorpho.org.")
 
 # Render!
 scene.render()
